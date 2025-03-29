d66809cf0cf58140385880df6f52196bd791b322
Hamad <AlmarzooqiH23@gmail.com>
2025-03-29 14:12:01 +0400

    Not really sure what ii have changed.

--- CPP02/ex00/main.cpp
+++ CPP02/ex00/main.cpp
@@ -6,19 +6,19 @@
 /*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 15:19:06 by hamad             #+#    #+#             */
-/*   Updated: 2025/03/20 15:19:24 by hamad            ###   ########.fr       */
+/*   Updated: 2025/03/26 03:51:03 by hamad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int main( void ) {
-Fixed a;
-Fixed b( a );
-Fixed c;
-c = b;
-std::cout << a.getRawBits() << std::endl;
-std::cout << b.getRawBits() << std::endl;
-std::cout << c.getRawBits() << std::endl;
-return 0;
+int main(void){
+    Fixed   a;
+    Fixed   b(a);
+    Fixed   c;
+    c = b;
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    return (0);
 }
