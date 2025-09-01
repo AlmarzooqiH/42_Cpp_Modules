0afcef68cbb9ec07d0bb2baed7901e2bdb7a7020
AlmarzooqiH <AlmarzooqiH23@gmail.com>
2025-09-01 20:02:02 +0400

    Added some exceptions.

--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: hamalmar <hamalmar@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 17:02:57 by hamalmar          #+#    #+#             */
-/*   Updated: 2025/08/28 17:06:50 by hamalmar         ###   ########.fr       */
+/*   Updated: 2025/08/31 00:32:38 by hamalmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,20 @@
 
 # include <iostream>
 # include <map>
+# include <exception>
+
+class BitcoinExchange{
+    public:
+        class InvalidInputException: public std::exception{
+            public:
+                const char *what() const throw();
+        };
+
+        class InvalidArgumentException: public std::exception{
+            public:
+                const char *what() const throw();
+        };
+};
 
 
 #endif
